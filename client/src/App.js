a96b054daee214d5dda6a07ebea527f1421a5a21
becover <sedi.september@gmail.com>
2020-09-24 11:59:52 +0900

    fix: 사이즈값 받아오는 이슈 고려않고 지웠다가 원복시켜놓음
    추후 불필요한 기능 및 코드 정리 필요

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,78 @@
-import React, { useState } from 'react';
+import React, {
+  useRef,
+  useEffect,
+  useCallback,
+  useState,
+  useLayoutEffect,
+} from 'react';
 import './App.css';
 import CanvasContainer from './containers/CanvasContainer';
 import NavContainer from './containers/NavContainer';
 import ToolsContainer from './containers/ToolsContainer';
+import { useDispatch } from 'react-redux';
+import { Width, Height } from './redux/modules/canvas';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const dispatch = useDispatch();
+  const onchangeWidth = useCallback((value) => dispatch(Width(value)), [
+    dispatch,
+  ]);
+
+  const onchangeHeight = useCallback((value) => dispatch(Height(value)), [
+    dispatch,
+  ]);
+
+  const Container = React.forwardRef((props, ref) => (
+    <div ref={ref}>{props.children}</div>
+  ));
+
+  const refs = {
+    header: useRef(),
+    footer: useRef(),
+  };
+
+  const { header, footer } = refs;
+  const [width, height] = UseWindowSize();
   const [initialSwitch, setInitialSwitch] = useState([
     { id: 'fill', checked: false },
     { id: 'border', checked: false },
   ]);
 
+  function UseWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([
+          window.innerWidth,
+          window.innerHeight -
+            header.current.offsetHeight -
+            footer.current.offsetHeight,
+        ]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+
+  useEffect(() => {
+    onchangeWidth(width);
+    onchangeHeight(height);
+    console.log(width, height);
+  }, [onchangeWidth, onchangeHeight, width, height, header, footer]);
+
   return (
     <div style={{ height: '100vh' }}>
-      <NavContainer />
-      <ToolsContainer initialSwitch={initialSwitch} />
+      <Container ref={header}>
+        <NavContainer />
+        <ToolsContainer initialSwitch={initialSwitch} />
+      </Container>
       <CanvasContainer setInitialSwitch={setInitialSwitch} />
-      <Footer />
+      <Container ref={footer}>
+        <Footer />
+      </Container>
     </div>
   );
 }
