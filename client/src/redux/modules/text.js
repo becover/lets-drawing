45b6f9610e177e4b06f6c965b0ac32e03894b476
becover <sedi.september@gmail.com>
2020-10-02 21:05:44 +0900

    typo fix initialState -> INITIAL_STATE로 변경

--- client/src/redux/modules/text.js
+++ client/src/redux/modules/text.js
@@ -2,11 +2,11 @@ const TEXT_MODE = 'paint/text/MOTEXT_MODEDE';
 
 export const text_mode = (mode) => ({ type: TEXT_MODE, mode });
 
-const initialState = {
+const INITIAL_STATE = {
   mode: null,
 };
 
-const text = (state = initialState, action) => {
+const text = (state = INITIAL_STATE, action) => {
   if (action.type === TEXT_MODE) {
     return {
       ...state,
