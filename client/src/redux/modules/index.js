a05236145e173606151d77ab8b66bd2f7cea4b4b
becover <sedi.september@gmail.com>
2020-10-02 21:15:31 +0900

    add tools

--- client/src/redux/modules/index.js
+++ client/src/redux/modules/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import canvas from './canvas';
+import tools from './tools';
 import history from './history';
 import drag from './drag';
 import text from './text';
 
-const rootReducer = combineReducers({ canvas, history, drag, text });
+const rootReducer = combineReducers({ canvas, tools, history, drag, text });
 
 export default rootReducer;
