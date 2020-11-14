e2b436e9e1b1a56a16cfd527e3bc41ae0886f842
becover <sedi.september@gmail.com>
2020-11-14 21:29:24 +0900

    feat: toast pupup animation, timer

--- client/src/Alert.js
+++ client/src/Alert.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import styled, { keyframes } from 'styled-components';
+import { modal } from './redux/modules/portal';
 
 const popup = keyframes`
 0%{
-top:0;
+top:3px;
 }
 50%{
-top:0%
+top:3px;
 }
 100%{
 top:-100%
@@ -16,7 +17,7 @@ top:-100%
 `;
 const AlertLayout = styled.div`
   padding: 20px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.4), 0 0 10px rgba(0, 0, 0, 0.4);
+  box-shadow: 0 0 5px rgba(50, 50, 50, 0.4), 0 0 10px rgba(50, 50, 50, 0.4);
   background: #fff;
   position: fixed;
   top: -100%;
@@ -24,14 +25,24 @@ const AlertLayout = styled.div`
   transform: translateX(-50%);
   border-radius: 5px;
   animation: ${popup} 3s linear;
+  p {
+    color: royalblue;
+  }
 `;
 
 export default function Alert() {
+  const dispatch = useDispatch();
   const { optionalProps } = useSelector(({ portal }) => ({
     optionalProps: portal.optionalProps,
   }));
+  const onModal = useCallback((state, compo) => dispatch(modal(state, compo)), [
+    dispatch,
+  ]);
+  const handleToast = () => {
+    setTimeout(() => onModal(false, null), 1000 * 3);
+  };
   return (
-    <AlertLayout>
+    <AlertLayout ref={(ref) => ref && handleToast()}>
       <p>{optionalProps.message}</p>
     </AlertLayout>
   );
