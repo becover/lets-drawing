a2a850ba2107b748a7d45278f49e6a27540ef8f6
becover <sedi.september@gmail.com>
2020-11-14 21:27:43 +0900

    feat: undo, redo shortkey
    undo: ctrl+z, redo: ctrl+shift+z

--- client/src/components/paintBrush/Tools/History/History.js
+++ client/src/components/paintBrush/Tools/History/History.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Undo from './Undo';
 import Redo from './Redo';
+import { useCallback } from 'react';
 
 function History({ onUndo, onRedo }) {
+  const handleShortKey = useCallback(
+    (e) => {
+      if (e.shiftKey && e.ctrlKey && e.code === 'KeyZ') return onRedo();
+      if (e.ctrlKey && e.code === 'KeyZ') return onUndo();
+    },
+    [onUndo, onRedo],
+  );
+  useEffect(() => {
+    window.addEventListener('keydown', handleShortKey);
+    return () => window.removeEventListener('keydown', handleShortKey);
+  }, [handleShortKey]);
   return (
     <>
       <Undo onUndo={onUndo} />
