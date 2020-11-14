917743cac45de4c8bb4668f0d536bd00457129c6
becover <sedi.september@gmail.com>
2020-11-14 21:44:54 +0900

    feat: login 여부로 redirect시키기

--- client/src/routes/Gallery.js
+++ client/src/routes/Gallery.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Redirect, useLocation } from 'react-router-dom';
 import GalleryContainer from '../containers/GalleryContainer';
 import NavContainer from '../containers/NavContainer';
 
 function Gallery() {
   const location = useLocation();
-
+  const { isLogged } = useSelector(({ auth }) => ({ isLogged: auth.isLogged }));
   return (
-    <div>
-      <NavContainer location={location.pathname} />
-      <GalleryContainer />
-    </div>
+    <>
+      {isLogged ? (
+        <div>
+          <NavContainer location={location.pathname} />
+          <GalleryContainer />
+        </div>
+      ) : (
+        <Redirect to="/" />
+      )}
+    </>
   );
 }
 
