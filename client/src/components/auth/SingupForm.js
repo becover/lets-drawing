a6f019534c07d430842281e9278d4c52c29589ab
becover <sedi.september@gmail.com>
2020-11-16 23:18:26 +0900

    feat: signup 기능 구현
    실시간 username중복 검사
    가입완료시 팝업메세지 출력

--- client/src/components/auth/SingupForm.js
+++ client/src/components/auth/SingupForm.js
@@ -1,20 +1,119 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import Alert from '../../Alert';
+const Checked = styled.span`
+  margin-left: 5px;
+  font-size: 0.5rem;
+  ${(props) => props.validations && `color:royalblue`}
+  ${(props) => !props.validations && `color:tomato`};
+`;
+export default function SingupForm({ onModal, onModalProps }) {
+  const refs = {
+    usernameRef: useRef(),
+    passwordRef: useRef(),
+    confirmPasswordRef: useRef(),
+  };
 
-export default function SingupForm() {
+  const [signUpInput, setSignUpInput] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [feedbackMassege, setFeedbackMassege] = useState('');
+  const [validations, setValidations] = useState({
+    validation: false,
+    available: false,
+  });
+
+  const onChangeInput = (e) => {
+    setSignUpInput({
+      ...signUpInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onCheckUsername = useCallback(() => {
+    const username = refs.usernameRef.current.value;
+    Axios.get(`http://localhost:4000/users/ckeckUsername?username=${username}`)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 404) {
+          setFeedbackMassege('사용할 수 없는 아이디 입니다.');
+        } else {
+          setFeedbackMassege(res.data.message);
+          setValidations({ validation: true, available: true });
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        setFeedbackMassege('사용할 수 없는 아이디 입니다.');
+        setValidations({ validation: true, available: false });
+      });
+  }, [refs.usernameRef]);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (!signUpInput.username) {
+      alert('아이디를 입력하세요');
+      refs.usernameRef.current.focus();
+    } else if (!signUpInput.password) {
+      alert('비밀번호를 입력하세요');
+      refs.passwordRef.current.focus();
+    } else if (!signUpInput.confirmPassword) {
+      alert('비밀번호를 확인해주세요');
+      refs.confirmPasswordRef.current.focus();
+    } else if (signUpInput.password !== signUpInput.confirmPassword) {
+      alert('비밀번호와 똑같이 입력해 주세요.');
+      refs.confirmPasswordRef.current.focus();
+    } else {
+      if (feedbackMassege === '가입 할 수 있는 아이디입니다.') {
+        const body = signUpInput;
+        Axios.post(`http://localhost:4000/users/signup`, body)
+          .then((res) => {
+            console.log(res.data);
+            onModalProps({ message: res.data.message });
+          })
+          .then(() => onModal(false, null))
+          .then(() => onModal(true, Alert))
+          .then(() => setSignUpInput({ username: '', password: '' }));
+      } else {
+        alert('아이디를 확인 해 주세요');
+      }
+    }
+  };
+
+  useEffect(() => {
+    refs.usernameRef.current && refs.usernameRef.current.focus();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    refs.usernameRef.current.addEventListener('input', onCheckUsername);
+    return () =>
+      refs.usernameRef.current.removeEventListener('input', onCheckUsername);
+  }, [onCheckUsername, refs.usernameRef]);
   return (
     <>
-      <form autoComplete="chrome-off">
+      <form autoComplete="chrome-off" onSubmit={onSubmitForm}>
         <fieldset>
           <legend>회원가입 화면</legend>
           <div>
             <p>
               <label htmlFor="username">ID</label>
+              <Checked validations={validations.available}>
+                {validations.validation && feedbackMassege}
+              </Checked>
             </p>
             <input
               type="text"
               id="username"
               placeholder="아이디"
-              ref={(ref) => ref && ref.focus()}
+              name="username"
+              ref={refs.usernameRef}
+              value={signUpInput.username}
+              onChange={onChangeInput}
               autoComplete="off"
             />
           </div>
@@ -22,7 +121,16 @@ export default function SingupForm() {
             <p>
               <label htmlFor="userPassword">PASSWORD</label>
             </p>
-            <input type="password" id="userPassword" placeholder="비밀번호" />
+            <input
+              type="password"
+              id="userPassword"
+              name="password"
+              placeholder="비밀번호"
+              ref={refs.passwordRef}
+              value={signUpInput.userPassword}
+              autoComplete="off"
+              onChange={onChangeInput}
+            />
           </div>
           <div>
             <p>
@@ -32,6 +140,11 @@ export default function SingupForm() {
               type="password"
               id="confirmPassword"
               placeholder="비밀번호 확인"
+              name="confirmPassword"
+              ref={refs.confirmPasswordRef}
+              value={signUpInput.confirmPassword}
+              onChange={onChangeInput}
+              autoComplete="off"
             />
           </div>
           <button>회원가입</button>
