64f04785fae31f48348af6ad92bb090f1d1b96fd
becover <sedi.september@gmail.com>
2020-11-16 23:20:00 +0900

    feat: auto login
    새로고침시 등 ls에 값이 있을 경우 서버에 인증 요청하여 자동로그인

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Gallery from './routes/Gallery';
 import Main from './routes/Main';
 import Axios from 'axios';
-
+import { useDispatch } from 'react-redux';
+import { login, logout } from './redux/modules/auth';
 export default function App() {
+  const dispatch = useDispatch();
+  const onLogin = useCallback(
+    (username) => {
+      dispatch(login(username));
+    },
+    [dispatch],
+  );
+
+  const onLogout = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+  // const byebye = (e) => {
+  //   e.preventDefault();
+  //   e.returnValue = '';
+  //   localStorage.removeItem('dw-token');
+  //   localStorage.removeItem('dw-user');
+  //   alert('정말 그만 그리실 건가요?');
+  // };
   useEffect(() => {
     Axios.get('http://localhost:4000/').then((res) => console.log(res.data));
+    // window.addEventListener('beforeunload', byebye);
   }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem('dw-token') && localStorage.getItem('dw-user')) {
+      const body = {
+        username: JSON.parse(localStorage.getItem('dw-user')),
+        Authorization: JSON.parse(localStorage.getItem('dw-token')),
+      };
+      Axios.post('http://localhost:4000/users/', body).then((res) => {
+        if (res.status === 200) onLogin(body.username);
+        else onLogout();
+      });
+    }
+  }, [onLogin, onLogout]);
   return (
     <>
       <Switch>
