0b6a8892b6eee0b9917cad55a630cb848c5a4c7b
becover <sedi.september@gmail.com>
2020-11-16 23:20:44 +0900

    style: add animation
    부드럽게 나타나는 애니메이션 스타일 추가

--- client/src/Portal.js
+++ client/src/Portal.js
@@ -2,13 +2,22 @@ import React, { useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { modal } from './redux/modules/portal';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+const smoothShow = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 const ModalLayout = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
   background: rgba(0, 0, 0, 0.6);
   z-index: 999;
+  animation: ${smoothShow} 0.6s ease-out;
   ${(props) => props?.optionalProps?.message && `background: rgba(0,0,0,.0)`}
 `;
 
