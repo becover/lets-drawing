eb94bef8fe306a7ab161c0b2c84927b073896a36
becover <sedi.september@gmail.com>
2020-11-18 22:38:53 +0900

    update: 불필요한 경로/파일제거

--- server/auth/isAuth.js
+++ server/auth/isAuth.js
@@ -1,7 +1,6 @@
 const config = require("../_config/config.json");
 const jwt = require("jsonwebtoken");
 const User = require("../users/user.model");
-const errorGenerator = require("../_helpers/errorGenerator");
 
 module.exports = async (req, res, next) => {
   try {
@@ -10,7 +9,7 @@ module.exports = async (req, res, next) => {
     const decodedToken = jwt.verify(token, config.SECRETKEY);
     const { _id } = decodedToken;
     const user = await User.findOne({ _id });
-    if (!user) return errorGenerator("사용자를 찾을 수 없습니다.", 404);
+    if (!user) next("사용자를 찾을 수 없습니다.");
     req.user = user;
     console.log("인증오켕");
     next();
