5ee174bea4b6383392889959b505551ee1154278
becover <sedi.september@gmail.com>
2020-11-21 23:16:49 +0900

    refactor: env lib 추가
    개발용코드와 배포용 코드로 분리

--- server/server.js
+++ server/server.js
@@ -1,24 +1,38 @@
 require("rootpath")();
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const errorHandler = require("./_helpers/error-handler");
+const config = require("./_config/config.json");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ limit: "2mb", extended: false }));
+process.env.NODE_ENV === "develop" &&
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }));
 app.use(
   bodyParser.json({
     limit: "30mb",
   })
 );
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 
-app.get("/", (req, res) => res.send("Connected to the server..."));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) =>
+    res.sendFile(__dirname, "../client", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => res.send("Connected to the server..."));
+}
 
 // api routes
 app.use("/users", require("./users/user.controller"));
@@ -27,8 +41,30 @@ app.use("/gallery", require("./gallery/gallery.controller"));
 // global error handler
 app.use(errorHandler);
 
-const port =
-  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-app.listen(port, function () {
-  console.log("Server listening on port " + port);
-});
+if (process.env.NODE_ENV === "production") {
+  const PORT =
+    process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
+
+  const option = {
+    ca: fs.readFileSync(config.ca),
+    key: fs.readFileSync(config.key),
+    cert: fs.readFileSync(config.cert),
+  };
+
+  http
+    .createServer(function (req, res) {
+      res.writeHead(301, {
+        Location: "https://" + req.headers["host"] + req.url,
+      });
+      res.end();
+    })
+    .listen(80);
+  https.createServer(option, app).listen(PORT, function () {
+    console.log("Server listening on port " + PORT);
+  });
+} else {
+  const port = 4000;
+  app.listen(port, function () {
+    console.log("Server listening on port " + port);
+  });
+}
